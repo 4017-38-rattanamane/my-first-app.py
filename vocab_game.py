20f3bc32d3774a828faaedf35407048ce7f9253c
4017-38-rattanamane <56873@yupparaj.ac.th>
2026-09-12 10:13:23 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -101,6 +101,14 @@ ans2 = st.text_input(
     "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
     value=st.session_state.ans2_val,
 )
+
+# อัปเดตค่าล่าสุดเข้าตัวแปร
+st.session_state.ans1_val = ans1
+st.session_state.ans2_val = ans2
+st.session_state.ans3_val = ans3
+st.session_state.ans4_val = ans4
+# ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มข้อ 3, 4 ตรงนี้
+
 ans3 = st.text_input(
     "ข้อ 3: `c_r`. 🚗",
     value=st.session_state.ans3_val,
@@ -109,13 +117,6 @@ ans4 = st.text_input(
     "ข้อ 4: `l_pt_p`. 💻",
     value=st.session_state.ans4_val,
 )
-
-# อัปเดตค่าล่าสุดเข้าตัวแปร
-st.session_state.ans1_val = ans1
-st.session_state.ans2_val = ans2
-st.session_state.ans3_val = ans3
-st.session_state.ans4_val = ans4
-# ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มข้อ 3, 4 ตรงนี้
 
 
 # 4. ปุ่มส่งคำตอบ
